5762709228683cc2754a2e9aa561d831fa27aa2e
Mike Graham <mgraham@technicalmedia.com>
2015-04-16 05:07:29 -0600

    dts-generator working !!!
    had to hack a normalize call into the dts-generator index.js file

--- dts-gen.js
+++ dts-gen.js
@@ -2,8 +2,9 @@ var dtsGenerator = require('dts-generator')
 
   debugger;
     dtsGenerator.generate({
-        name: 'logging',
+        name: 'aurelia-logging',
         baseDir: '/a/aurelia-ts-port/aurelia-ts/logging',
         files: [ 'index.d.ts' ],
-          out: 'logging.d.ts'
+          out: 'logging.d.ts',
+          main: 'index.ts'
     });
