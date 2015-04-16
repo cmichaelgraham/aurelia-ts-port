3069d022476a9871da9ce3a0c5d3d9537c77b051
Mike Graham <mgraham@technicalmedia.com>
2015-04-16 15:42:41 -0600

    added build data structure to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,51 @@ var ts = require('gulp-typescript');
 var merge = require('merge2');
 var dtsGenerator = require('dts-generator')
 
+var buildTargets = {
+  rings: [
+    { name: 'ring-0', repos: [
+      { name: 'event-aggregator' },
+      { name: 'history' },
+      { name: 'html-template-element' },
+      { name: 'logging' },
+      { name: 'logging-console' },
+      { name: 'path' },
+      { name: 'metadata' },
+      { name: 'skeleton-plugin' },
+      { name: 'task-queue' },
+      { name: 'route-recognizer'}
+      ]},
+    { name: 'ring-1', repos: [
+      { name: 'dependency-injection' },
+      { name: 'history-browser' },
+      { name: 'loader' },
+      { name: 'http-client' }
+    ]},
+    { name: 'ring-2', repos: [
+      { name: 'binding' },
+      { name: 'router' },
+      { name: 'loader-default' }
+    ]},
+    { name: 'ring-3', repos: [
+      { name: 'templating' }
+    ]},
+    { name: 'ring-4', repos: [
+      { name: 'framework' },
+      { name: 'animator-css' },
+      { name: 'templating-binding' },
+      { name: 'templating-resources' },
+      { name: 'templating-router' }
+    ]},
+    { name: 'ring-5', repos: [
+      { name: 'app-contacts' },
+      { name: 'bootstrapper' },
+      { name: 'skeleton-navigation' }
+    ]}
+  ]
+}
+
 gulp.task('dts-gen', function() {
+  debugger;
     dtsGenerator.generate({
         name: 'logging',
         baseDir: '/c/a/aurelia-ts-port/aurelia-ts/logging',
