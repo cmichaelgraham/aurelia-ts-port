51fd4d78b85ee60d88664a98fb4e1eb3d7f981e7
Mike Graham <mgraham@technicalmedia.com>
2015-04-16 05:29:07 -0600

    fix formatting

--- dts-gen.js
+++ dts-gen.js
@@ -1,10 +1,9 @@
 var dtsGenerator = require('dts-generator')
 
-  debugger;
-    dtsGenerator.generate({
-        name: 'aurelia-logging',
-        baseDir: '/a/aurelia-ts-port/aurelia-ts/logging',
-        files: [ 'index.d.ts' ],
-          out: 'logging.d.ts',
-          main: 'aurelia-logging/index'
-    });
+dtsGenerator.generate({
+    name: 'aurelia-logging',
+    baseDir: '/a/aurelia-ts-port/aurelia-ts/logging',
+    files: [ 'index.d.ts' ],
+    out: 'logging.d.ts',
+    main: 'aurelia-logging/index'
+});
