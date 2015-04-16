8f37ff00ff797e8195202a6e6f73bfccf137730d
Mike Graham <mgraham@technicalmedia.com>
2015-04-16 07:38:59 -0600

    working for animator-css

--- dts-gen.js
+++ dts-gen.js
@@ -1,9 +1,17 @@
 var dtsGenerator = require('dts-generator')
 
 dtsGenerator.generate({
+    name: 'aurelia-animator-css',
+    baseDir: '/a/aurelia-ts-port/aurelia-ts/animator-css',
+    files: [ 'index.ts', 'animator.ts' ],
+    out: 'animator-css.d.ts',
+    main: 'aurelia-animator-css/index'
+});
+
+dtsGenerator.generate({
     name: 'aurelia-logging',
     baseDir: '/a/aurelia-ts-port/aurelia-ts/logging',
-    files: [ 'index.d.ts' ],
+    files: [ 'index.ts' ],
     out: 'logging.d.ts',
     main: 'aurelia-logging/index'
 });
