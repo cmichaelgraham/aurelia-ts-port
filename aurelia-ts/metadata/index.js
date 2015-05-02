e2cf7342c2addc9b577ea1c5b4c944f997b3d6ae
Mike Graham <mgraham@technicalmedia.com>
2015-05-01 19:34:13 -0600

    inprocess with metadata

--- aurelia-ts/metadata/index.js
+++ aurelia-ts/metadata/index.js
@@ -3,16 +3,8 @@
  *
  * @module metadata
  */
-var __decorate = this.__decorate || (typeof Reflect === "object" && Reflect.decorate) || function (decorators, target, key, desc) {
-    switch (arguments.length) {
-        case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
-        case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
-        case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
-    }
-};
-define(["require", "exports", './origin', './resource-type', './metadata', './decorators'], function (require, exports, origin_1, resource_type_1, metadata_1, decorators_1) {
+define(["require", "exports", './origin', './metadata', './decorators'], function (require, exports, origin_1, metadata_1, decorators_1) {
     exports.Origin = origin_1.Origin;
-    exports.ResourceType = resource_type_1.ResourceType;
     exports.Metadata = metadata_1.Metadata;
     exports.Decorators = decorators_1.Decorators;
 });
